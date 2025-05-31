bd046711001e40ceda26a256b628e357c7a6f4ec
dolprat <dolpratxyz@gmail.com>
2025-06-01 01:55:52 +0700

    change some of blog/document/page.js

--- app/blog/[documentId]/page.js
+++ app/blog/[documentId]/page.js
@@ -5,27 +5,28 @@ const fetchBlog = async (documentId) => {
     const response = await axios.get(
       `${process.env.STRAPI_BASE_URL}/api/blogs/${documentId}/?populate[0]=thumbnail&populate[1]=author`
     );
-
     return response.data.data;
   } catch (error) {
     console.log("error", error);
-    return [];
+    return null;
   }
 };
 
 export default async function Page({ params }) {
-  const blogdocumentId = params.documentId;
-  const blog = await fetchBlog(blogdocumentId);
-
+  const blog = await fetchBlog(params.documentId);
+  if (!blog) {
+    return <div>Blog not found.</div>;
+  }
   return (
     <div>
       Blog ID: {blog.id}
       <img
         width="100px"
         src={`${process.env.STRAPI_BASE_URL}${blog.thumbnail.url}`}
+        alt={blog.title}
       />
       <div>{blog.title}</div>
-      <div>author by: {blog.author.name}</div>
+      <div>author by: {blog.author.name ?? "Unknown"}</div>
     </div>
   );
 }
